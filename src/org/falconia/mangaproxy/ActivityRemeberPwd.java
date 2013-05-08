8907e0677f3cfca97cfee740f78df500da0acbcf
vertusd <vertusd@localhost>
2013-05-08 19:55:58 +0800

    add

--- src/org/falconia/mangaproxy/ActivityRemeberPwd.java
+++ src/org/falconia/mangaproxy/ActivityRemeberPwd.java
@@ -9,6 +9,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Looper;
+import android.os.StrictMode;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
@@ -72,7 +74,62 @@ public class ActivityRemeberPwd extends Activity {
 		return mSite.getDisplayname();
 	}
 	
+	private void startLoginThread() {
+		new Thread(downloadRun).start();  
+	}
 	
+	private void showSccuessMessage(){
+		Toast.makeText(this, "登陆成功，正在获取用户数据……", Toast.LENGTH_SHORT).show();
+	}
+	
+	private void showFailedMessage(){
+		Toast.makeText(this, "密码错误，请重新输入", Toast.LENGTH_SHORT).show();
+	}
+	
+	private void startActivityGenreList(){
+		if (mSite.hasGenreList()) {
+			ActivityGenreList.IntentHandler.startActivityGenreList(ActivityRemeberPwd.this, mSiteId);
+		} else {
+			ActivityMangaList.IntentHandler.startActivityAllMangaList(ActivityRemeberPwd.this, mSiteId);
+		}
+		finish();
+	}
+	
+	/**
+	 * 下载线程
+	 */
+	Runnable downloadRun = new Runnable(){
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+			Looper.prepare();
+			cardNumStr = cardNumAuto.getText().toString();
+			passwordStr = passwordET.getText().toString();
+			
+			
+			if (Plugins.getPlugin(getSiteId()).login(cardNumStr,passwordStr))
+			{
+				if (savePasswordCB.isChecked()) {// 登陆成功才保存密码
+					sp.edit().putString(cardNumStr, "").commit();
+				}
+				AppUtils.logV(this, "login Sucessfully!");
+				showSccuessMessage();
+				startActivityGenreList();
+
+			
+			}
+			else
+			{
+				showFailedMessage();
+				AppUtils.logV(this, "login failed!");
+			}
+		}
+	};
+	
+    
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -152,6 +209,10 @@ public class ActivityRemeberPwd extends Activity {
 		});
 		
 		
+		
+		
+		
+		
 		// 登陆
 		logBT.setOnClickListener(new OnClickListener() {
 
@@ -160,32 +221,7 @@ public class ActivityRemeberPwd extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
                 
-				cardNumStr = cardNumAuto.getText().toString();
-				passwordStr = passwordET.getText().toString();
-				
-				
-				if (Plugins.getPlugin(getSiteId()).login(cardNumStr,passwordStr))
-				{
-					if (savePasswordCB.isChecked()) {// 登陆成功才保存密码
-						sp.edit().putString(cardNumStr, "").commit();
-					}
-					AppUtils.logV(this, "login Sucessfully!");
-					Toast.makeText(ActivityRemeberPwd.this, "登陆成功，正在获取用户数据……", Toast.LENGTH_SHORT).show();
-					
-					
-					if (mSite.hasGenreList()) {
-						ActivityGenreList.IntentHandler.startActivityGenreList(ActivityRemeberPwd.this, mSiteId);
-					} else {
-						ActivityMangaList.IntentHandler.startActivityAllMangaList(ActivityRemeberPwd.this, mSiteId);
-					}
-					finish();
-				
-				}
-				else
-				{
-					Toast.makeText(ActivityRemeberPwd.this, "密码错误，请重新输入", Toast.LENGTH_SHORT).show();
-					AppUtils.logV(this, "login failed!");
-				}
+               startLoginThread();
 
 			}
 		});
